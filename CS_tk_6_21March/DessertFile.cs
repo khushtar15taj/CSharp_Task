72f964bdc445bedf978c6876924c36983d424c5b
khushtar15taj <36955712+khushtar15taj@users.noreply.github.com>
2022-04-02 02:49:11 +0530

    Add files via upload

---
+++ CS_tk_6_21March/DessertFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+namespace CS_tk_6_21March
+{
+    public class DessertFile
+    {
+       
+        public void DessertWriter()
+        {
+            var fileName = @"D:\Files.txt";
+            List<string> desserts = new List<string>();
+            desserts.Add("BlacForest");
+            desserts.Add("Chocolava");
+           
+
+            FileStream desserfile = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter dessertStream = new StreamWriter(desserfile);
+            foreach (string dessert in desserts)
+            {
+                dessertStream.Write(dessert + "\n");
+            }
+
+            dessertStream.Flush();
+            dessertStream.Close();
+            desserfile.Close();
+        }
+
+        public void DesserReader()
+        {
+            var fileName = @"D:\Files.txt";
+            FileStream dessertReaderFile = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader DessertReaderStream = new StreamReader(dessertReaderFile);
+            String outputData = DessertReaderStream.ReadToEnd();
+            DessertReaderStream.Close();
+            dessertReaderFile.Close();
+
+            Console.WriteLine(outputData);
+            FileInfo info = new FileInfo(fileName);
+            Console.WriteLine(info.Length);
+            Console.WriteLine(info.CreationTime);
+            
+        }
+    }
+    
+}
---
+++ CS_tk_6_21March/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CS_tk_6_21March
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DessertFile df = new DessertFile();
+            df.DessertWriter();
+            df.DesserReader();
+
+            Employee emp = new Employee();
+            emp.Id = 1;
+            emp.Name = "krish";
+            emp.Age = 25;
+            emp.Gender = "female";
+
+            EmployeeFile efile = new EmployeeFile();
+            efile.WriteToBinaryFile(emp);
+            efile.Deserialize();
+            Console.Read();
+
+        }
+    }
+}
