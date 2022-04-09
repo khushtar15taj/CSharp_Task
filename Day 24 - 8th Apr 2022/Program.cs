1af04c194f7a85fd2df96dbf2fcb731caa626cbf
khushtar15taj <36955712+khushtar15taj@users.noreply.github.com>
2022-04-09 18:19:58 +0530

    Add files via upload

---
+++ Day 24 - 8th Apr 2022/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Day_24___8th_Apr_2022
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int legs = Convert.ToInt32(Console.ReadLine()); ;
+            int eyes = Convert.ToInt32(Console.ReadLine()); ;
+            int goats, ducks;
+            int total = eyes / 2;
+            goats  = (legs - eyes) / 2;
+            ducks = total - goats;
+
+            Console.WriteLine("the Number of goats and ducks are as follows respectively {0}, {1}" ,goats,ducks);
+        }
+    }
+}
