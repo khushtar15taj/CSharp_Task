4e374aa1cc6233bc6cba42ae3f24d645390ffe1c
khushtar15taj <36955712+khushtar15taj@users.noreply.github.com>
2022-04-13 03:06:42 +0530

    Add files via upload

---
+++ 23rd MArch - Script/RegistrationForm.cs
@@ -0,0 +1,61 @@
+﻿
+ <!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="RegistrationFormStyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+
+    <script src="RegistrationFormScript.js"></script>
+
+</head>
+
+
+
+<body>
+    <form name="myform" action="Index.html" onsubmit="return validate()">
+        <fieldset>
+            <h1>Registration Form</h1>
+            <label>Enter your Fisrt name</label><br>
+            <input type="text" name="fname">
+            <p id="fnameloc" style="color:red"></p>
+
+            <label>Enter your Last Name</label><br>
+            <input type="text" name="lname"><br>
+            <p id="lnameloc" style="color:red"></p>
+
+            <label>Enter your Email</label><br>
+            <input type="text" name="email">
+            <p id="emailloc" style="color:red"></p>
+            <p>
+                <label>Enter your Password</label><br>
+                <input type="password" name="psw" id="password" />
+            <p id="passwordloc" style="color:red"></p>
+            <i class="bi bi-eye-slash" id="togglePassword"></i>
+            </p>
+            <label>Enter your Age </label><br>
+            <input type="text" name="age" style="width:10%"><br>
+            <p id="ageloc" style="color:red"></p>
+            <input type="submit" value="submit">
+        </fieldset>
+    </form>
+
+</body>
+<script>
+    const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#password");
+
+    togglePassword.addEventListener("click", function () {
+        console.log(3);
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        this.classList.toggle("bi-eye");
+    });
+</script>
+
+
+</html>
+ 
---
+++ 23rd MArch - Script/RegistrationFormScript.cs
@@ -0,0 +1,68 @@
+﻿function validate()
+{
+    var fname = document.myform.fname.value;
+    var lname = document.myform.lname.value;
+    var ages = document.myform.age.value;
+    var emails = document.myform.email.value;
+    var password = document.myform.psw.value;
+    var validRegex = / ^[a - zA - Z0 - 9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var passwordPattern = / (?=.*\d)(?=.*[a - z])(?=.*[A - Z]).{ 8,}/;
+
+    console.log(4);
+
+    if (!isNaN(fname) || fname == "")
+    {
+        document.getElementById("fnameloc").innerHTML = "Enter valid first name";
+        document.getElementById("togglePassword").style.top = "400px";
+        return false;
+    }
+    else
+    {
+        document.getElementById("fnameloc").innerHTML = "";
+    }
+    if (!isNaN(lname) || lname == "")
+    {
+        document.getElementById("lnameloc").innerHTML = "Enter valid last name";
+        document.getElementById("togglePassword").style.top = "400px";
+        return false
+    }
+    else
+    {
+        document.getElementById("lnameloc").innerHTML = "";
+    }
+    if (emails == "" || !(emails.match(validRegex)))
+    {
+        document.getElementById("emailloc").innerHTML = "Enter valid Email";
+        document.getElementById("togglePassword").style.top = "400px";
+        return false;
+    }
+    else
+    {
+        document.getElementById("emailloc").innerHTML = "";
+    }
+    console.log(password);
+    if (password == "" || !((String(password)).match(passwordPattern)))
+    {
+        document.getElementById("passwordloc").innerHTML = " Password Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters";
+        document.getElementById("togglePassword").style.top = "365px";
+        return false;
+    }
+    else
+    {
+        document.getElementById("passwordloc").innerHTML = "";
+    }
+
+    if (isNaN(ages) || ages == "")
+    {
+        document.getElementById("ageloc").innerHTML = "Enter valid Age";
+        document.getElementById("password").style.top = "465px";
+        return false;
+    }
+    else
+    {
+        document.getElementById("ageloc").innerHTML = "";
+    }
+
+    return true;
+
+}
