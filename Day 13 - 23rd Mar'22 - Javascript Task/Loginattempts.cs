58df801a3fea4eec4d253ed4d32f4d7d6eba28ed
khushtar15taj <36955712+khushtar15taj@users.noreply.github.com>
2022-04-13 02:56:25 +0530

    Add files via upload

---
+++ Day 13 - 23rd Mar'22 - Javascript Task/Loginattempts.cs
@@ -0,0 +1,52 @@
+﻿
+  <!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+    <meta charset="utf-8" />
+    <title></title>
+    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+
+    <script src="loginjs.js"></script>
+
+</head>
+
+<!--// action="Index.html"-->
+
+<body>
+    <form id="form1"  action="Index.html" onsubmit="return validate()">
+        <fieldset>
+            <h1>Registration Form</h1>
+
+
+            <label>UserName</label><br>
+            <input id="name" type="text" name="email">
+            <p style="color:red"></p>
+            <p>
+                <label>Password</label><br>
+                <input type="password" name="psw" id="password" />
+            <p style="color:red"></p>
+            <i class="bi bi-eye-slash" id="togglePassword"></i>
+            </p>
+
+            <input type="submit" value="Sign-up" id="hide_id">
+        </fieldset>
+    </form>
+
+</body>
+<script>
+    const togglePassword = document.querySelector("#togglePassword");
+    const password = document.querySelector("#password");
+
+    togglePassword.addEventListener("click", function () {
+        console.log(3);
+        const type = password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
+        this.classList.toggle("bi-eye");
+    });
+</script>
+
+
+</html>
---
+++ Day 13 - 23rd Mar'22 - Javascript Task/loginjs.cs
@@ -0,0 +1,51 @@
+﻿var login_attempts = 3;
+function validate()
+{
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("password").value;
+    console.log(pass);
+    if (!name && !pass)
+    {
+        return false;
+    }
+    else
+    {
+
+        if (name == "khushtar" && pass == "khushtar")
+        {
+            document.getElementById("name").value = "";
+            document.getElementById("password").value = "";
+            return true;
+
+        }
+        else
+        {
+            if (login_attempts == 0)
+            {
+                alert("No Login Attempts Available");
+                console.log(8);
+                document.getElementById("name").disabled = true;
+                document.getElementById("password").disabled = true;
+                document.getElementById("form1").disabled = true;
+                document.getElementById('hide_id').style.cursor = 'none';
+            }
+            else
+            {
+                login_attempts = login_attempts - 1;
+                alert("Login Failed Now Only " + login_attempts + " Login Attempts Available");
+                if (login_attempts == 0)
+                {
+                    console.log(2);
+                    document.getElementById("name").disabled = true;
+                    document.getElementById("password").disabled = true;
+                    document.getElementById("form1").disabled = true;
+                    document.getElementById('hide_id').style.cursor = 'none';
+
+                }
+            }
+        }
+
+    }
+    return false;
+
+}
