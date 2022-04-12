0e66150e0c2591f7fa8d03cd93a0c0dd61490842
khushtar15taj <36955712+khushtar15taj@users.noreply.github.com>
2022-04-13 02:35:12 +0530

    Add files via upload

---
+++ Day 2 Task - HTML/Recipe.cs
@@ -0,0 +1,84 @@
+﻿< !DOCTYPE html >
+ 
+
+ < html lang = "en" xmlns = "http://www.w3.org/1999/xhtml" >
+    < head >
+    
+        < meta charset = "utf-8" />
+     
+         < link rel = "stylesheet" href = "https://www.w3schools.com/w3css/4/w3.css" >
+        
+            < link rel = "stylesheet" href = "DessertStyle.css" >
+           
+
+               < title > Recipe </ title >
+           </ head >
+           < body >
+           
+               < div class= "w3-container w3-teal" id = "Heading" >
+             
+                     < p > Recipes </ p >
+             
+                 </ div >
+             
+                 < h2 class= "headingdessert" > Desserts with Ingredients list</h2>
+
+    <dl>
+        <div class= "container" >
+        < dt > Rasmalai </ dt >
+        < dd style = "width:20%" >< img src = "D:\DessertImages\Rasmalai.png" alt = "Rasmalai" style = "width:100%" ></ dd >
+         
+                 < dt > Rasmalai Ingredients </ dt >
+            
+                    < dd >< img src = "D:\DessertImages\Rasmalai_Ingredients.png" alt = "Rasmalai" style = "width:40%" ></ dd >
+                   
+                           < ul style = "width:20%" >
+                    
+                                < dt > Ingredients </ dt >
+                    
+                                < li > 1 tsb Vanilla Extract</li>
+            <li>3 cup Powdered Sugar</li>
+            <li>2 cup Heavy Cream</li>
+            <li>10Pistachios</li>
+            <li>1 cup Rasmalai milk</li>
+            <li>1 tsbCardamon powder</li>
+            <li>10 Sliced Almonds</li>
+            <li>2 cup Yogurt</li>
+            <li>3 cup Oil</li>
+            <br>
+        </ul>
+        </div>
+
+        <div class= "container2" >
+        < dt > Sheer Khurma </ dt >
+   
+           < dd >< img src = "D:\DessertImages\Sheer_Khurma.png" alt = "Rasmalai" style = "width:50%" ></ dd >
+          
+                  < dt > Sheer khurma Ingredients</dt>
+                     <dd><img src = "D:\DessertImages\Sheer_Khurma_Ingridients.png" alt= "Rasmalai" style= "width:50%" ></ dd >
+             
+                      < ul style= "width:20%" >
+             
+                         < dt > Ingredients </ dt >
+             
+                         < li > Vermicille </ li >
+             
+                         < li > 3 Saffron</li>
+                         <li>2 cup Ghee</li>
+            <li>Dates</li>
+                         <li>Cloves</li>
+                         <li>Sugar</li>
+                     </ul>
+        </div>
+
+    </dl>
+    <footer>
+        <p class= "w3-container w3-teal" >
+            The Recipe list<br>
+            <a href = "mailto:hege@example.com" > hege@example.com</a>
+        </p>
+    </footer>
+
+
+</body>
+</html>
